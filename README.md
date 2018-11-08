cd556b4133b995579be8eacd4f1daf21107c3a9b
nabriski <itamar.n@mobfox.com>
2018-11-08 10:03:19 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+echo "{\"a\":1}" | script="console.log(JSON.parse(stdin).a)" gadwall
+1
+
---
+++ index.js
@@ -0,0 +1,13 @@
+const getStdin  = require("get-stdin");
+const {NodeVM} = require('vm2');
+
+getStdin().then(str => {
+
+    const vm = new NodeVM({
+        console: 'inherit',
+        sandbox: {stdin:str}
+    });
+
+    vm.run(process.env.script);
+
+});
